f1d2441fa101cc99f9026f3bc9ab07d3435a1dd6
Andreas Broennimann <andreas.broennimann@v-net.ch>
2012-03-30 09:59:56 +0200

    Initial commit

---
+++ src/chat.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  chat.c
+ *
+ *    Description:  A primitive chat system.
+ *
+ *        Version:  1.0
+ *        Created:  03/30/2012 09:48:38 AM
+ *       Revision:  {node|short}
+ *       Compiler:  gcc
+ *
+ *         Author:  Andreas Brönnimann (v-net.ch), 
+ *        Company:  v-net.ch
+ *
+ * =====================================================================================
+ */
+
+#include	<stdlib.h>
+#include	<stdio.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  Obviously the main function to start the chat.
+ * =====================================================================================
+ */
+    int
+main ( int argc, char *argv[] )
+{
+    return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
